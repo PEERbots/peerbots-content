19845af3bdd3d5230ffc43faa94af0585de91d01
Yasser Nadeem <ok.yasser@gmail.com>
2021-12-19 13:45:23 -0500

    Attempt to fix CI/CD

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  assetPrefix: isProd ? '/peerbots-content/' : ''
+const nextConfig = {
+  assetPrefix: isProd ? '/peerbots-content/' : '',
+  images: {
+    loader: 'akamai',
+    path: ''
+  },
 }
+
+module.exports = nextConfig
