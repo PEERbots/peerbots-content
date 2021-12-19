40ff252be5a98f4ec07a4d8440b8ba36c3ed2c0e
Saad Elbeleidy <sbeleidy90@gmail.com>
2021-12-19 16:53:11 -0500

    Start developing auth modal #6
    Use Dialog from headlessui
    Add design guidelines font and colors
    Add input class based on designs
    Add tailwind forms for good form defaults
    Add svg for icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#46D9D9",
+        "dark-primary": "#5FC7CC",
+        secondary: "#E86E8A", // red
+        accent: "#D9E021", // light-yellow
+        "accent-hc": "#C4CC24", // dark-yellow
+        "accent-two": "#4273FF", // blue
+        "accent-two-hc": "#516EB5", // dark blue
+        "accent-three": "#3F8588",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    }),
+  ],
+};
