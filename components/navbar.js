1d443a9d28440344942b902db413ecf709546223
Yasser Nadeem <ok.yasser@gmail.com>
2021-12-24 22:36:29 -0500

    Corrects class to className

--- components/navbar.js
+++ components/navbar.js
@@ -67,11 +67,11 @@ export default function Navbar() {
             <div className="inset-0">
               <div className="">
                 {/* Search Bar */}
-                <div class="flex items-center justify-center">
-                  <div class="flex border-2 rounded">
-                      <input type="text" class="px-4 py-2 w-80" placeholder="Search..."></input>
-                        <button class="flex items-center justify-center px-4 border-l">
-                            <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                <div className="flex items-center justify-center">
+                  <div className="flex border-2 rounded">
+                      <input type="text" className="px-4 py-2 w-80" placeholder="Search..."></input>
+                        <button className="flex items-center justify-center px-4 border-l">
+                            <svg className="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 24 24">
                               <path
                                   d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
