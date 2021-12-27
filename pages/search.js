e087020e977b3af4c09dfab0207fd2dd57f30bd0
Saad Elbeleidy <sbeleidy90@gmail.com>
2021-12-27 12:46:45 -0500

    add search results using algolia

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,46 @@
 import ContentRow from "../components/contentRow";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
+import algoliaApp from "../algolia";
+import firebaseApp from "../firebase";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  limit,
+  documentId,
+  getDocs,
+} from "firebase/firestore";
 
 export default function SearchResults() {
   const router = useRouter();
   const [searchResults, setSearchResults] = useState([]);
   const { q } = router.query;
+  const contentIndex = algoliaApp.initIndex("Content Index");
+  const db = getFirestore(firebaseApp);
 
   const fetchSearchResults = async () => {
     if (q) {
-      console.log(q);
+      const contentResults = await contentIndex.search(q);
+      const contentIds = contentResults.hits.map((contentHit) => {
+        return contentHit.objectID;
+      });
+
+      const q = query(
+        collection(db, "content"),
+        where("public", "==", true),
+        where(documentId(), "in", contentIds),
+        limit(10)
+      );
+      const data = await getDocs(q);
+      const searchResultsFromDb = data.docs.map((doc) => {
+        return {
+          id: doc.id,
+          data: doc.data(),
+        };
+      });
+      setSearchResults(searchResultsFromDb);
     }
   };
 
