389abbb7f9377bd53c63a4cdfef46c2b1811efe8
Saad Elbeleidy <sbeleidy90@gmail.com>
2021-12-29 12:00:45 -0500

    Add review cards and rating to the summary

--- pages/content/[contentId].js
+++ pages/content/[contentId].js
@@ -211,7 +211,7 @@ export default function ContentPage() {
   return (
     <div>
       <div className="bg-white shadow-md my-4 mx-2 rounded p-4">
-        <div className="">
+        <div>
           <h1 className="text-2xl">{contentInfo.name}</h1>
         </div>
         <div>
@@ -237,7 +237,41 @@ export default function ContentPage() {
             <div></div>
           )}
         </div>
-        <div>
+        <div className="my-2">
+          {!contentInfo.copyOf && reviews ? (
+            <>
+              {reviews.length > 0 ? (
+                <span>
+                  <span className="text-accent-hc font-bold px-2 text-base">
+                    {reviews.reduce((sum, { data }) => {
+                      return sum + data.rating;
+                    }, 0) / reviews.length}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-4 w-4 inline-block"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                      />
+                    </svg>
+                  </span>
+                  <span className="text-sm">({reviews.length} reviews)</span>
+                </span>
+              ) : (
+                <span className="text-xs">No reviews yet</span>
+              )}
+            </>
+          ) : (
+            <span>{""}</span>
+          )}
+        </div>
+        <div className="my-2">
           {tags &&
             tags.map((eachTag) => (
               <span
@@ -257,45 +291,64 @@ export default function ContentPage() {
         <div>{contentInfo.description}</div>
       </div>
 
-      <div className="bg-white shadow-md my-4 mx-2 rounded p-4 ">
-        {!contentInfo.copyOf ? (
-          <div>
-            {reviews.length > 0 ? (
-              <>
-                <h3>Reviews</h3>
+      {!contentInfo.copyOf ? (
+        <>
+          {reviews.length > 0 ? (
+            <div className="bg-white shadow-md my-4 mx-2 rounded p-4 ">
+              <div>
+                <h3 className="text-sm font-bold">Reviews</h3>
                 {reviews.map((review) => (
-                  <div key={review.id}>
-                    <div>{review.data.rating}</div>
-                    <div>
+                  <div
+                    key={review.id}
+                    className="bg-white shadow-lg rounded p-4 w-64"
+                  >
+                    <div className="flex justify-between mb-2">
                       <div>
-                        <img
-                          src={
+                        {review.data.rating}{" "}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-4 w-4 inline-block"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                        </svg>
+                      </div>
+
+                      <div className="align-middle">
+                        <span>
+                          <img
+                            src={
+                              reviewers.filter((reviewer) => {
+                                return reviewer.id == review.data.user.id;
+                              })[0].data.photoUrl
+                            }
+                            className="rounded-full h-6 w-6 inline-block mr-1"
+                          ></img>
+                        </span>
+                        <span className="text-sm">
+                          {
                             reviewers.filter((reviewer) => {
                               return reviewer.id == review.data.user.id;
-                            })[0].data.photoUrl
+                            })[0].data.name
                           }
-                        ></img>
-                      </div>
-                      <div>
-                        {
-                          reviewers.filter((reviewer) => {
-                            return reviewer.id == review.data.user.id;
-                          })[0].data.name
-                        }
+                        </span>
                       </div>
                     </div>
-                    <div>{review.data.description}</div>
+                    <div className="text-sm text-gray-700">
+                      {review.data.description}
+                    </div>
                   </div>
                 ))}
-              </>
-            ) : (
-              <></>
-            )}
-          </div>
-        ) : (
-          <div>{""}</div>
-        )}
-      </div>
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
+        </>
+      ) : (
+        <div>{""}</div>
+      )}
 
       <div>
         <div>
