7a52f51bc6b1a2b61d98bb29d98b2953f38136d2
Saad Elbeleidy <sbeleidy90@gmail.com>
2021-12-29 10:37:55 -0500

    style: start moving things around for content details page

--- pages/content/[contentId].js
+++ pages/content/[contentId].js
@@ -210,30 +210,128 @@ export default function ContentPage() {
 
   return (
     <div>
-      <div>{contentInfo.name}</div>
-      <div>
-        {author ? (
-          <div className="text-gray-800">
-            <span>
-              {author.photoUrl ? (
-                <img
-                  src={author.photoUrl}
-                  className="h-8 inline-block rounded-full"
-                ></img>
-              ) : (
-                <img
-                  src="profile_pic.png"
-                  className="h-8 inline-block rounded-full"
-                ></img>
-              )}
-            </span>
-            <span className="ml-1 text-xs">{author.name}</span>
+      <div className="bg-white shadow-md my-4 mx-2 rounded p-4">
+        <div className="">
+          <h1 className="text-2xl">{contentInfo.name}</h1>
+        </div>
+        <div>
+          <span className="text-xs">Authored by</span>
+          {author ? (
+            <div className="text-gray-800">
+              <span>
+                {author.photoUrl ? (
+                  <img
+                    src={author.photoUrl}
+                    className="h-8 inline-block rounded-full"
+                  ></img>
+                ) : (
+                  <img
+                    src="profile_pic.png"
+                    className="h-8 inline-block rounded-full"
+                  ></img>
+                )}
+              </span>
+              <span className="ml-1 text-base">{author.name}</span>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+        <div>
+          {tags &&
+            tags.map((eachTag) => (
+              <span
+                key={eachTag.id}
+                style={{
+                  background: eachTag.data.color,
+                  color: eachTag.data.textColor,
+                }}
+                className="rounded-3xl px-2 mx-1 text-xs"
+              >
+                {eachTag.data.name}
+              </span>
+            ))}
+        </div>
+      </div>
+      <div className="bg-white shadow-md my-4 mx-2 rounded p-4 ">
+        <div>{contentInfo.description}</div>
+      </div>
+
+      <div className="bg-white shadow-md my-4 mx-2 rounded p-4 ">
+        {!contentInfo.copyOf ? (
+          <div>
+            {reviews.length > 0 ? (
+              <>
+                <h3>Reviews</h3>
+                {reviews.map((review) => (
+                  <div key={review.id}>
+                    <div>{review.data.rating}</div>
+                    <div>
+                      <div>
+                        <img
+                          src={
+                            reviewers.filter((reviewer) => {
+                              return reviewer.id == review.data.user.id;
+                            })[0].data.photoUrl
+                          }
+                        ></img>
+                      </div>
+                      <div>
+                        {
+                          reviewers.filter((reviewer) => {
+                            return reviewer.id == review.data.user.id;
+                          })[0].data.name
+                        }
+                      </div>
+                    </div>
+                    <div>{review.data.description}</div>
+                  </div>
+                ))}
+              </>
+            ) : (
+              <></>
+            )}
           </div>
         ) : (
-          <div></div>
+          <div>{""}</div>
         )}
       </div>
+
       <div>
+        <div>
+          {!contentInfo.copyOf && reviews ? (
+            <>
+              {reviews.length > 0 ? (
+                <span>
+                  <span className="text-accent-hc font-bold px-2 text-base">
+                    {reviews.reduce((sum, { data }) => {
+                      return sum + data.rating;
+                    }, 0) / reviews.length}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-4 w-4 inline-block"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                      />
+                    </svg>
+                  </span>
+                  <span className="text-sm">({reviews.length} reviews)</span>
+                </span>
+              ) : (
+                <span className="text-xs">No reviews yet</span>
+              )}
+            </>
+          ) : (
+            <span>{""}</span>
+          )}
+        </div>
         <div>
           {contentInfo.copyOf ? (
             <span>
@@ -318,90 +416,7 @@ export default function ContentPage() {
           </div>
         )}
       </div>
-      <div>
-        {!contentInfo.copyOf && reviews ? (
-          <>
-            {reviews.length > 0 ? (
-              <span>
-                <span className="text-accent-hc font-bold px-2 text-base">
-                  {reviews.reduce((sum, { data }) => {
-                    return sum + data.rating;
-                  }, 0) / reviews.length}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4 inline-block"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                    />
-                  </svg>
-                </span>
-                <span className="text-sm">({reviews.length} reviews)</span>
-              </span>
-            ) : (
-              <span className="text-xs">No reviews yet</span>
-            )}
-          </>
-        ) : (
-          <span>{""}</span>
-        )}
-      </div>
-      <div>
-        {tags &&
-          tags.map((eachTag) => (
-            <span
-              key={eachTag.id}
-              style={{
-                background: eachTag.data.color,
-                color: eachTag.data.textColor,
-              }}
-              className="rounded-3xl px-2 mx-1 text-xs"
-            >
-              {eachTag.data.name}
-            </span>
-          ))}
-      </div>
-      <div>{contentInfo.description}</div>
-      <div>
-        {!contentInfo.copyOf ? (
-          <div>
-            <h3>Reviews</h3>
-            {reviews.length > 0 &&
-              reviews.map((review) => (
-                <div key={review.id}>
-                  <div>{review.data.rating}</div>
-                  <div>
-                    <div>
-                      <img
-                        src={
-                          reviewers.filter((reviewer) => {
-                            return reviewer.id == review.data.user.id;
-                          })[0].data.photoUrl
-                        }
-                      ></img>
-                    </div>
-                    <div>
-                      {
-                        reviewers.filter((reviewer) => {
-                          return reviewer.id == review.data.user.id;
-                        })[0].data.name
-                      }
-                    </div>
-                  </div>
-                  <div>{review.data.description}</div>
-                </div>
-              ))}
-          </div>
-        ) : (
-          <div>{""}</div>
-        )}
-      </div>
+
       <div>
         {copies.length > 0 ? (
           <div>
