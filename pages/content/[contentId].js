5107fc0e4b43a103b1e371e063d8a34eb9c72577
Saad Elbeleidy <sbeleidy90@gmail.com>
2021-12-31 14:30:52 -0500

    Refactor summaryRating Fixes #50

--- pages/content/[contentId].js
+++ pages/content/[contentId].js
@@ -19,6 +19,7 @@ import amplitude from "amplitude-js";
 import Link from "next/link";
 import ContentRow from "../../components/contentRow";
 import { Rating } from "@mui/material";
+import SummaryRating from "../../components/summaryRating";
 
 import { useFirebaseAuth } from "../../auth";
 
@@ -407,37 +408,8 @@ export default function ContentPage() {
           )}
         </div>
         <div className="my-2">
-          {!contentInfo.copyOf && contentInfo.public && reviews ? (
-            <>
-              {reviews.length > 0 ? (
-                <span>
-                  <span className="text-accent-hc font-bold px-2 text-base">
-                    {reviews.reduce((sum, { data }) => {
-                      return sum + data.rating;
-                    }, 0) / reviews.length}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-4 w-4 inline-block"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                      />
-                    </svg>
-                  </span>
-                  <span className="text-sm">({reviews.length} reviews)</span>
-                </span>
-              ) : (
-                <span className="text-xs">No reviews yet</span>
-              )}
-            </>
-          ) : (
-            <span>{""}</span>
+          {!contentInfo.copyOf && contentInfo.public && reviews && (
+            <SummaryRating reviews={reviews} />
           )}
         </div>
         <div className="my-2">
@@ -629,31 +601,8 @@ export default function ContentPage() {
       {contentInfo.public && !contentInfo.copyOf && (
         <div className="bg-white shadow-md my-4 mx-2 rounded p-4 ">
           <div>
-            {reviews && reviews.length > 0 ? (
-              <span>
-                <span className="text-accent-hc font-bold px-2 text-base">
-                  {reviews.reduce((sum, { data }) => {
-                    return sum + data.rating;
-                  }, 0) / reviews.length}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4 inline-block"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                    />
-                  </svg>
-                </span>
-                <span className="text-sm">({reviews.length} reviews)</span>
-              </span>
-            ) : (
-              <span className="text-xs">No reviews yet</span>
+            {!contentInfo.copyOf && contentInfo.public && reviews && (
+              <SummaryRating reviews={reviews} />
             )}
           </div>
           <div>
