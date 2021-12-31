aaa346108cc5dc317bb07cfd0631b6026172e843
Yasser Nadeem <ok.yasser@gmail.com>
2021-12-31 17:23:04 -0500

    Completes card responsive styling

--- components/contentRow.js
+++ components/contentRow.js
@@ -14,7 +14,7 @@ import firebaseApp from "../firebase";
 
 export default function ContentRow({ content, children }) {
   const [authors, setAuthors] = useState([]);
-  const [ratings, setRatings] = useState([]);
+  const [reviews, setReviews] = useState([]);
   const [tags, setTags] = useState([]);
   content.forEach((content) => {
     content.data.id = content.id;
@@ -80,28 +80,11 @@ export default function ContentRow({ content, children }) {
         const docData = doc.data();
         return {
           content: docData.content.id,
-          rating: docData.rating,
+          data: docData,
         };
       });
 
-      const ratingsFromDb = contentReferences.map((contentId) => {
-        const reviewsForContent = reviewsFromDb.filter((review) => {
-          return review.content == contentId.id;
-        });
-        const nReviews = reviewsForContent.length;
-        return {
-          contentId: contentId.id,
-          averageRating:
-            nReviews > 0
-              ? reviewsForContent.reduce((sum, { rating }) => {
-                  return sum + rating;
-                }, 0)
-              : null,
-          nReviews: nReviews,
-        };
-      });
-
-      setRatings(ratingsFromDb);
+      setReviews(reviewsFromDb);
     }
   };
   useEffect(() => {
@@ -123,11 +106,9 @@ export default function ContentRow({ content, children }) {
                     return author.id == eachContent.data.owner.id;
                   })[0]
                 }
-                rating={
-                  ratings.filter((rating) => {
-                    return rating.contentId == eachContent.id;
-                  })[0]
-                }
+                reviews={reviews.filter((review) => {
+                  return review.content == eachContent.id;
+                })}
                 tags={tags.filter((tag) => {
                   return eachContent.data.tags
                     .map((t) => {
