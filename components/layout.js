488a41e72f4166ef7b87e86f0714a621a63f5594
Saad Elbeleidy <sbeleidy90@gmail.com>
2022-01-01 13:07:46 -0500

    Add footer styling
    Make social icons consistent
    Make footer stick to the bottom
    Add github social icon

--- components/layout.js
+++ components/layout.js
@@ -6,11 +6,11 @@ export default function Layout({ children }) {
   return (
     <>
       <FirebaseAuthProvider>
-        <div className="bg-white shadow-md my-4 mx-2 rounded">
-          <Navbar />
-        </div>
-        {children}
-        <div className="bg-white shadow-md my-4 mx-2 rounded">
+        <div className="flex flex-col min-h-screen">
+          <div className="bg-white shadow-md my-4 mx-2 rounded">
+            <Navbar />
+          </div>
+          <div className="flex-grow">{children}</div>
           <Footer />
         </div>
       </FirebaseAuthProvider>
